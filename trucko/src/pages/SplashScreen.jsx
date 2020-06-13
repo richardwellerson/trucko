e4a39546f0678a195b4802341f1209dca322b96d
richardwellerson <richardwellerson@gmail.com>
2020-06-13 01:49:24 -0300

    Retirado da Splash Screen

--- trucko/src/pages/SplashScreen.jsx
+++ trucko/src/pages/SplashScreen.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import Header from '../components/Header';
 
 const SplashScreen = () => {
   return (
     <div>
-      <Header />
+      <p>SplashScreen</p>
     </div>
   );
 }
