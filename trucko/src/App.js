0f64aba49ca34ddc28c15c12b8b73b8f5925591d
nayararn <nayaranascimento0112@gmail.com>
2020-06-13 03:21:45 -0300

    Adicionando footer

--- trucko/src/App.js
+++ trucko/src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
 import "./App.css";
-import { useContext } from "react";
 
 function App() {
-  // const { name, setName } = useContext(Trucko);
-  return (
-    <div className="App">
-      <p>Trucko</p>
-    </div>
-  );
+  return <p>Trucko</p>;
 }
 
 export default App;
