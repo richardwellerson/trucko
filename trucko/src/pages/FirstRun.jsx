31a1aa9cddf80c388f2be18994f4c9d5b41ef384
nayararn <nayaranascimento0112@gmail.com>
2020-06-13 22:38:52 -0300

    Alterando css

--- trucko/src/pages/FirstRun.jsx
+++ trucko/src/pages/FirstRun.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import Header from '../components/Header';
-import Welcome from '../components/Welcome';
-import StartButton from '../components/StartButton';
-import StartDay from '../components/StartDay';
-import Feels from '../components/Feels';
-import Footer from '../components/Footer';
-import '../styles/FirstRun.css';
+import React from "react";
+import Header from "../components/Header";
+import Welcome from "../components/Welcome";
+import StartButton from "../components/StartButton";
+import StartDay from "../components/StartDay";
+import Feels from "../components/Feels";
+import Footer from "../components/Footer";
+import "../styles/FirstRun.css";
 
 const FirstRun = () => {
   return (
-    <div className="first_run_all">
-      <Header />
-      <Welcome />
-      <StartDay />
-      <Feels />
-      <StartButton />
-      <Footer />
-    </div>
+    <>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="first_run_all">
+        <Welcome />
+        <StartDay />
+        <Feels />
+        <StartButton />
+        <Footer />
+      </div>
+    </>
   );
 };
 
